b7389703f8fd2f418dc2e60a81c18fbbe527fcec
backup-for-rl <878741420@qq.com>
2026-09-25 16:09:25 +0800

    Make Linux test discovery independent of installed tests packages

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+"""Project-local regression tests.
+
+Keeping this directory an explicit package prevents an unrelated installed
+package named ``tests`` from shadowing these modules on the Linux server.
+"""
